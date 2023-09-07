2841cca50881606092f4b323b60b9e2077a69d75
windows_on_wsl <otomijuf.004@gmail.com>
2023-09-07 22:51:10 +0900

    fix yielder

--- tools/preprocess_data.py
+++ tools/preprocess_data.py
@@ -171,7 +171,7 @@ def yield_from_files(fnames: list, semaphore):
 
     def hf_yielder(fname, semaphore):
         stream = filter(lambda x: x, lmd.Reader(fname).stream_data())    
-        for f in filter(lambda x: 'text' in x and len(json.leads(x)['text']) != 0, stream):
+        for f in filter(lambda x: 'text' in x and len(json.loads(x)['text']) != 0, stream):
             semaphore.acquire()
             yield f['text']
 
