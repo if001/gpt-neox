0be6712fbea8ee142f15f971b6652e388e4a511d
windows_on_wsl <otomijuf.004@gmail.com>
2023-09-24 19:56:35 +0900

    debug

--- tools/convert_module_to_hf_gptneox2.py
+++ tools/convert_module_to_hf_gptneox2.py
@@ -146,10 +146,9 @@ def convert(input_checkpoint_path, loaded_config, output_checkpoint_path):
 
     hf_config = create_config(loaded_config)
 
-    # hf_model = GPTNeoXForCausalLM(hf_config)
+    # hf_model = GPTNeoXForCausalLM(hf_config)    
     ## for swiglu
-    # hf_model = GPTNeoX2ForCausalLM(hf_config)
-    hf_model = AutoModelForCausalLM.from_config(hf_config)
+    hf_model = GPTNeoX2ForCausalLM(hf_config)    
 
     # save model in fp16/bf16 if Deepspeed fp16 or bf16 mixed precision was used in config, else 32 bit weights
     fp16 = get_key(loaded_config, "fp16")
